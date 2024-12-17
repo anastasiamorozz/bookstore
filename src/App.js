9d31dd58580db01d4511e05b7612ad6f679d23ae
Olia Sereda <sereda745@gmail.com>
2024-12-17 17:05:42 +0200

    add component about us

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Header from './components/Header/Header';
 import MainBanner from './components/MainBanner/MainBanner';
 import { NewBooks } from './components/NewBooks/NewBooks';
 import AuthorBanner from './components/AuthorBanner/AuthorBanner';
 import DiscountBanner from './components/DiscountBanner/DiscountBanner';
-import Footer from "./components/Footer/Footer";
+import Footer from './components/Footer/Footer';
+import AboutUs from './components/AboutUs/AboutUs';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <MainBanner />
-      <NewBooks/>
+      <NewBooks />
       <AuthorBanner />
-      <DiscountBanner/> 
-      <Footer/>
+      <DiscountBanner />
+      <Footer />
     </div>
   );
 }
